ec37f4f78792e2f59c0c37dd25dcd8ba7730676c
Vedakshi Korde <vedakshi.12@gmail.com>
2025-04-16 02:02:59 +0530

    Update StarPattern7.java

--- StarPattern7.java
+++ StarPattern7.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 class StarPattern7{
+	//pyramid star pattern
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		
+
+		//outer for loop
 		for(int i=1;i<=n;i++){
 			for(int j=i;j<n;j++){
 				System.out.print(" ");
@@ -13,7 +15,7 @@ class StarPattern7{
 			}
 			System.out.println();
 		}
-		
+		//prints inverted pyramid star pattern
 		for(int i=n;i>=1;i--){
 			for(int j=i;j<n;j++){
 				System.out.print(" ");
