8b1a3d3ed4f2b7ba012fdb58ef3730dba486356e
Vedakshi Korde <vedakshi.12@gmail.com>
2025-04-23 15:53:22 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -3,11 +3,15 @@ class Sum{
 	public static void main(String args[]){
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		int sum=0;
-
+		int sum=0;//inialization
+		
+		//this loop iterates till it reaches to the nth number 
 		for(int i=1;i<=n;i++){
+			//this will simply add the numbers 
+			//for ex: sum=0 and i=1 then sum+i is 0+1 is 1 and value gets stored inside sum
 			sum = sum+i;
 		}
+		//Prints the sum which is stored in sum variable after the loop condition becomes false 
 		System.out.println("Sume is " +sum);	
 	}
 }
